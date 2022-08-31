bb36eb65557f9462ec8acef6ceca0e50b8299684
sangyoon kim <sangyun0904@gmail.com>
2022-08-31 18:43:12 +0900

    Add W5 function_prototype.c , scores_pointer.c and swap.c

---
+++ W5_pointer_array/swap.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void swap(int *i, int *j)
+{
+    int temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
+int main()
+{
+    int a, b;
+    a = 1;
+    b = 2;
+
+    printf("a = %d and b = %d\n", a, b);
+
+    swap(a, b);
+
+    printf("a = %d and b = %d\n", a, b);
+
+    return 0;
+}
