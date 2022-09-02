d51f4399e5eecd6e2551a4c74709e9a7bcf2fd86
sangyoon kim <sangyun0904@gmail.com>
2022-09-02 16:22:41 +0900

    Add enum , preprocessor and assert practice c

---
+++ C_structured_programming/W1/assert_prac.c
@@ -0,0 +1,10 @@
+#include <assert.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main() 
+{
+    assert(0);
+    printf("My program runs\n");
+    return 0;
+}
---
+++ C_structured_programming/W1/assert_prac2.c
@@ -0,0 +1,11 @@
+#define NDEBUG
+#include <assert.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main() 
+{
+    assert(0);
+    printf("My program runs\n");
+    return 0;
+}
