05546d3c6b2aec374dba33656cbd94ab41f4f907
sangyoon kim <sangyun0904@gmail.com>
2023-11-29 16:42:20 +0900

    Add read aasx and convert into text file

--- readAAS/readAAS.c
+++ readAAS/readAAS.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 /* Our structure */
 struct rec
@@ -10,19 +11,26 @@ int main()
 {
     int counter;
     FILE *ptr_myfile;
+    FILE *w_myfile;
     struct rec my_record;
 
     ptr_myfile=fopen("01_Festo.aasx","rb");
+    w_myfile=fopen("01_Festo.txt", "w");
     if (!ptr_myfile)
     {
         printf("Unable to open file!");
         return 1;
     }
     for ( counter=1; counter <= 10; counter++)
+    while (true)
     {
         fread(&my_record,sizeof(struct rec),1,ptr_myfile);
-        printf("%d\n",my_record.x);
+        if (my_record.x == NULL) {
+            break;
+        }
+        fprintf(w_myfile, "%d\n",my_record.x);
     }
     fclose(ptr_myfile);
+    fclose(w_myfile);
     return 0;
 }
